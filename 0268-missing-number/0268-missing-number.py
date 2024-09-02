2e38c010ede0e4e5fc51d4ab52d6e8ba2fb03d71
Arslan Ali <105268642+arslanali19470@users.noreply.github.com>
2024-09-03 02:52:46 +0500

    Time: 88 ms (88.58%), Space: 12.9 MB (38.75%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,7 +1,12 @@
 class Solution(object):
     def missingNumber(self, nums):
-        for num in range(len(nums)+1):
-            if nums.count(num)==0:
-                return num
+        # for num in range(len(nums)+1):
+        #     if nums.count(num)==0:
+        #         return num
+        num=len(nums)
+        TotalSum=num*(num+1)//2
+        actual_sum=sum(nums)
+        return TotalSum-actual_sum
+
 
         
