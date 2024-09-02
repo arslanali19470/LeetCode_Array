464bbefa220869cb9f56a70370ba79cfde665ce4
Arslan Ali <105268642+arslanali19470@users.noreply.github.com>
2024-09-03 02:14:51 +0500

    Time: 6101 ms (5.08%), Space: 13.4 MB (40.39%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,13 +1,8 @@
 class Solution(object):
     def majorityElement(self, nums):
-        MainObj = {}
         for num in nums:
-            if num in MainObj:
-                MainObj[num]+=1
-            else:
-                MainObj[num]=1
-            
-            if MainObj[num]>len(nums)/2:
+            if nums.count(num)>len(nums)/2:
                 return num
+
         
 
